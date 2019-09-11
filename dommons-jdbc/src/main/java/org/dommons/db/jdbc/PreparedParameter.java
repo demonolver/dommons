0667b1029bc404a90491a0c218f7d173805ebe6f
demon <demon.basara@gmail.com>
2019-09-11 15:21:16 +0800

    优化byte[]值SQL拼装

--- dommons-jdbc/src/main/java/org/dommons/db/jdbc/PreparedParameter.java
+++ dommons-jdbc/src/main/java/org/dommons/db/jdbc/PreparedParameter.java
@@ -3,15 +3,56 @@
  */
 package org.dommons.db.jdbc;
 
+import java.nio.charset.Charset;
 import java.sql.Types;
 import java.util.Date;
 import org.dommons.core.convert.Converter;
+import org.dommons.core.string.Stringure;
+import org.dommons.security.coder.B64Coder;
 
 /**
  * 预编译参数
  * @author Demon 2012-2-11
  */
 abstract class PreparedParameter {
+
+	/**
+	 * 转换 Base64
+	 * @param bs 字节集
+	 * @return Base64 串
+	 */
+	static String base4(byte[] bs) {
+		return "from_base64('" + B64Coder.encodeBuffer(bs) + ')';
+	}
+
+	/**
+	 * 字节转换字符串
+	 * @param bs 字节集
+	 * @return 字符串
+	 */
+	static String string(byte[] bs) {
+		try {
+			return Stringure.toString(bs, Charset.defaultCharset());
+		} catch (RuntimeException e) {
+			return null;
+		}
+	}
+
+	/**
+	 * 转换字符串
+	 * @param value 值
+	 * @return 字符串
+	 */
+	static String string(Object value) {
+		if (value instanceof byte[]) {
+			byte[] bs = (byte[]) value;
+			String s = string(bs);
+			if (s == null) s = base4(bs);
+			return s;
+		} else {
+			return Converter.P.convert(value, String.class);
+		}
+	}
 
 	private final int type;
 	private final Object value;
@@ -28,62 +69,69 @@ abstract class PreparedParameter {
 
 	/**
 	 * 拼合参数
-	 * @param buffer 缓冲区
+	 * @param buf 缓冲区
 	 */
-	public void merge(StringBuilder buffer) {
+	public void merge(StringBuilder buf) {
 		switch (type) {
 		case Types.BIT:
 		case Types.TINYINT:
 		case Types.BIGINT:
 		case Types.INTEGER:
 		case Types.SMALLINT:
-			SQLFormatter.appendString(toString(Long.class), buffer);
+			SQLFormatter.appendString(toString(Long.class), buf);
 			break;
 
 		case Types.FLOAT:
 		case Types.DOUBLE:
 		case Types.DECIMAL:
-			SQLFormatter.appendString(toString(Number.class), buffer);
+			SQLFormatter.appendString(toString(Number.class), buf);
 			break;
 
 		case Types.BOOLEAN:
-			SQLFormatter.appendString(toString(Boolean.class), buffer);
+			SQLFormatter.appendString(toString(Boolean.class), buf);
 			break;
 
 		case Types.DATE:
 		case Types.TIME:
 		case Types.TIMESTAMP:
-			SQLFormatter.appendDate(value, buffer, type, getType(), getVersion());
+			SQLFormatter.appendDate(value, buf, type, getType(), getVersion());
 			break;
 
-		case Types.VARBINARY:
 		case Types.VARCHAR:
 		case Types.CHAR:
-		case Types.BINARY:
-			buffer.append('\'');
-			SQLFormatter.appendString(toString(String.class), buffer);
-			buffer.append('\'');
+			buf.append('\'');
+			SQLFormatter.appendString(toString(String.class), buf);
+			buf.append('\'');
 			break;
 
 		default:
 			if (type == VersionAdapter.type("NVARCHAR", -9) || type == VersionAdapter.type("LONGNVARCHAR", -16)) {
-				buffer.append('\'');
-				SQLFormatter.appendString(toString(String.class), buffer);
-				buffer.append('\'');
+				buf.append('\'');
+				SQLFormatter.appendString(toString(String.class), buf);
+				buf.append('\'');
 			} else if (type == VersionAdapter.type("SQLXML", 2009)) {
 				try {
-					if (VersionAdapter.instanceOf(value, "java.sql.SQLXML"))
-						SQLFormatter.appendString(Converter.P.convert(
-							VersionAdapter.invoke(value, VersionAdapter.find(VersionAdapter.findClass("java.sql.SQLXML"), "getString")),
-							String.class), buffer);
+					if (VersionAdapter.instanceOf(value, "java.sql.SQLXML")) SQLFormatter.appendString(Converter.P.convert(
+						VersionAdapter.invoke(value, VersionAdapter.find(VersionAdapter.findClass("java.sql.SQLXML"), "getString")),
+						String.class), buf);
 				} catch (RuntimeException e) {
-					SQLFormatter.appendString(toString(String.class), buffer);
+					SQLFormatter.appendString(toString(String.class), buf);
 				}
 			} else if (type == VersionAdapter.type("ROWID", -8)) {
 				SQLFormatter.appendString(VersionAdapter.instanceOf(value, "java.sql.RowId") ? value.toString() : toString(String.class),
-					buffer);
+					buf);
+			} else if (value instanceof byte[]) {
+				byte[] bs = (byte[]) value;
+				String s = string(bs);
+				if (s != null) {
+					buf.append('\'');
+					SQLFormatter.appendString(s, buf);
+					buf.append('\'');
+				} else {
+					SQLFormatter.appendString(base4(bs), buf);
+				}
 			} else {
-				SQLFormatter.appendString(toString(String.class), buffer);
+				SQLFormatter.appendString(toString(String.class), buf);
 			}
 
 			break;
@@ -122,11 +170,11 @@ abstract class PreparedParameter {
 					} catch (RuntimeException e) {
 					}
 				}
-				return toString(String.class);
+				return toString(Object.class);
 			} else if (type == VersionAdapter.type("ROWID", -8)) {
-				return VersionAdapter.instanceOf(value, "java.sql.RowId") ? value.toString() : toString(String.class);
+				return VersionAdapter.instanceOf(value, "java.sql.RowId") ? value.toString() : toString(Object.class);
 			} else {
-				return toString(String.class);
+				return toString(Object.class);
 			}
 		}
 	}
@@ -149,12 +197,12 @@ abstract class PreparedParameter {
 	 * @return 字符串
 	 */
 	protected String toString(Class cls) {
-		Object value = null;
+
+		Object value = this.value;
 		try {
-			value = Converter.P.convert(this.value, cls);
+			if (String.class.equals(cls)) value = Converter.P.convert(this.value, cls);
 		} catch (RuntimeException e) {
-			value = this.value;
 		}
-		return Converter.P.convert(value, String.class);
+		return string(value);
 	}
 }
