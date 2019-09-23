98fb96fc748c13b93c20ba7a8c6ad9aea4d6b8c5
demon <demon.basara@gmail.com>
2019-09-23 16:49:47 +0800

    调整 jackson 版本适配

--- dommons-io/src/main/java/org/dommons/io/json/JacksonVersions.java
+++ dommons-io/src/main/java/org/dommons/io/json/JacksonVersions.java
@@ -5,21 +5,28 @@ package org.dommons.io.json;
 
 import org.dommons.core.util.beans.ObjectInstantiators;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.PropertyNamingStrategyBase;
 
 /**
+ * 版本适配
  * @author demon 2018-06-11
  */
 class JacksonVersions {
 
-	@SuppressWarnings("deprecation")
 	protected static PropertyNamingStrategyBase snake() {
 		try {
-			return ObjectInstantiators.newInstance(Class.forName("com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy",
-				false, ObjectMapper.class.getClassLoader()));
+			return (PropertyNamingStrategyBase) PropertyNamingStrategy.SNAKE_CASE;
 		} catch (Throwable t) {
-			return new com.fasterxml.jackson.databind.PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy();
+			t.printStackTrace();
+		}
+		try {
+			return ObjectInstantiators
+					.newInstance(Class.forName("com.fasterxml.jackson.databind.PropertyNamingStrategy$LowerCaseWithUnderscoresStrategy",
+						false, PropertyNamingStrategy.class.getClassLoader()));
+		} catch (Throwable t) {
+			t.printStackTrace();
+			return null;
 		}
 	}
 }
