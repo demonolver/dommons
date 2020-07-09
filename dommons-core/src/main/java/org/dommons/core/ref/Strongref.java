603cb2493628f8b834b9ee58566d5e36cfb1bc96
demon <demon.basara@gmail.com>
2020-07-09 11:07:58 +0800

    强引用支持可序列化

--- dommons-core/src/main/java/org/dommons/core/ref/Strongref.java
+++ dommons-core/src/main/java/org/dommons/core/ref/Strongref.java
@@ -3,11 +3,15 @@
  */
 package org.dommons.core.ref;
 
+import java.io.Serializable;
+
 /**
  * 强引用 使用中不做释放
  * @author demon 2018-08-22
  */
-public class Strongref<T> implements Ref<T> {
+public class Strongref<T> implements Ref<T>, Serializable {
+
+	private static final long serialVersionUID = 5097156840789700965L;
 
 	/** 空对象引用 */
 	public static final Strongref empty = new Strongref(null);
